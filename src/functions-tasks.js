12db7fc3f3ad2491068d81dbb07f6fff9dbf56d2
Olena <olena.lightness@gmail.com>
2025-03-26 13:45:23 +0200

    feat: solve the problem getArgumentsCount

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -54,8 +54,10 @@ function getFunctionBody(func) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  return funcs.map((func) => {
+    return func.length;
+  });
 }
 
 /**
